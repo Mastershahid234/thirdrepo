c68ce2652eaedb63ff945a9e2417e726170cb48f
jollyllb123 <shahid06h@gmail.com>
2023-03-14 21:56:44 +0530

    third commit

---
+++ src/cermati/arr1.java
@@ -0,0 +1,15 @@
+package cermati;
+import java.util.*;
+public class arr1 {
+	  public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ 
+  int i=5;
+  System.out.println(--i);
+	  }
+}
+
+
+	
+
+
